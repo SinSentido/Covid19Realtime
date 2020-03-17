9095b32a509637818380e95df25177b144076f60
SinSentido <sinsentido993@gmail.com>
2020-03-17 14:06:58 +0100

    added navigation

--- app/src/main/java/com/example/covid19realtime/MainActivity.kt
+++ app/src/main/java/com/example/covid19realtime/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.covid19realtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    val navController: NavController by lazy {
+        Navigation.findNavController(this, R.id.navHostFragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
